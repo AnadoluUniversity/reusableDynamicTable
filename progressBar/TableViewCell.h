d3bc0c7a51e0e47e3778551ea2864e999fc6b9a1
mac <mac@macbooks-MacBook-Pro.local>
2014-03-12 15:48:32 +0200

    d

---
+++ progressBar/TableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.h
+//  progressBar
+//
+//  Created by mac on 11/03/14.
+//  Copyright (c) 2014 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UIButton *startButton;
+
+@property (nonatomic, strong) IBOutlet UILabel *progressLabel;
+@property (nonatomic, strong) UIProgressView *progressView;
+@property (nonatomic, strong) NSTimer *myTimer;
+@end
