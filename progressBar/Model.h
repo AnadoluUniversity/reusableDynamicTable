6a77488d32e7902289da9805784220fa837abc3c
Erk EKİN <erkekin@gmail.com>
2014-03-28 22:38:47 +0200

    some refactoring

--- progressBar/Model.h
+++ progressBar/Model.h
@@ -13,13 +13,12 @@
 
 @property (readwrite) float progress;
 
-@property (strong, nonatomic)  NSRunLoop * runLoop;
 @property (strong, nonatomic)  AFHTTPRequestOperation *operation;
 @property (readwrite)  BOOL isRunning;
 @property (readwrite) NSString *title;
 @property (readwrite) NSString *link;
 
 - (void)startDownload:(NSURLRequest*)request andVC:(ViewController*)vc;
-- (id)initWithBlock:(void (^)(void))blockName;
+- (id)init;
 
 @end
--- progressBar/Model.m
+++ progressBar/Model.m
@@ -1,39 +1,55 @@
-    //
-    //  Model.m
-    //  progressBar
-    //
-    //  Created by Erk EKIN on 19/03/14.
-    //  Copyright (c) 2014 mac. All rights reserved.
-    //
+//
+//  Model.m
+//  progressBar
+//
+//  Created by Erk EKIN on 19/03/14.
+//  Copyright (c) 2014 mac. All rights reserved.
+//
 
 #import "Model.h"
 
-
 @implementation Model
 
+- (id)init
+
+{
+    if (self = [super init]) {
+    
+    self.progress = 0;
+    self.isRunning = NO;
+    self.link= @"";
+    self.title= @"";
+    
+}
+    return self;
+}
+
 - (void)startDownload:(NSURLRequest*)request andVC:(ViewController*)vc{
+    self.isRunning = YES;
     
     __weak typeof(self) weakSelf = self;
- self.operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    self.operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
     
-     weakSelf.operation.responseSerializer = [AFImageResponseSerializer serializer];
+    weakSelf.operation.responseSerializer = [AFImageResponseSerializer serializer];
     
     [ self.operation setDownloadProgressBlock:^(NSUInteger bytesRead, NSInteger totalBytesRead, NSInteger totalBytesExpectedToRead) {
         
         float progress = ((float)totalBytesRead) / totalBytesExpectedToRead;
-      
-        NSLog(@"%f",progress);
         
-       weakSelf.progress=progress;
+        weakSelf.progress=progress;
         
         [vc updateCellForModel:weakSelf];
+        
     }];
     
-    [ self.operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [self.operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
         
         weakSelf.operation=nil;
+        weakSelf.isRunning = NO;
+        
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         
+        weakSelf.isRunning = NO;
         
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error"
                                                             message:[error localizedDescription]
@@ -43,26 +59,9 @@
         [alertView show];
     }];
     
-    [ self.operation start];
-
-}
-
-
-
-- (id)initWithBlock:(void (^)(void))blockName
-
-{ if (self = [super init]) {
+    [self.operation start];
     
-        self.progress = 0;
-        self.isRunning = NO;    
-        self.link= @"";
-        self.title= @"";
-        self.runLoop = [NSRunLoop currentRunLoop];
-
 }
-    return self;
-}
-
 
 
 @end
